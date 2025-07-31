d1ae23e3e5e63c50e9c0e6d50865e2eaeaff37cb
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-01 00:27:17 +0400

    added a quick projects description

--- Eng-Master/src/App.jsx
+++ Eng-Master/src/App.jsx
@@ -1,7 +1,15 @@
 export default function App(){
   return(
-    <header className="main-header">
-        Eng-Master
-    </header>
+    <>
+      <header className="main-header">
+        <img src="images/gear-icon.svg" alt="gear-icon" />
+        <h1>Eng-Master</h1>
+      </header>
+      <section className="quick-description">
+          <h2>
+            Turn your ideas into reality <br /> by first of all knwoing everything you need
+          </h2>
+      </section>
+    </>
   )
 }
