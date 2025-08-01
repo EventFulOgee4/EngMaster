16a99cd6d1ef4dee98a6ce2e27af46880f38fa50
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-01 08:23:52 +0400

    added the input + button to submit details

--- Eng-Master/src/App.jsx
+++ Eng-Master/src/App.jsx
@@ -1,6 +1,16 @@
+import React from "react"
+
 export default function App(){
+  const dots = React.useRef(null)
+
+  React.useEffect(() => {
+    const dotLine = ".".repeat(500) + "\n"
+    dots.current.textContent = dotLine.repeat(200)
+  })
+
   return(
     <>
+      <div className="background" ref={dots}></div>
       <header className="main-header">
         <img src="images/gear-icon.svg" alt="gear-icon" />
         <h1>Eng-Master</h1>
@@ -10,6 +20,12 @@ export default function App(){
             Turn your ideas into reality <br /> by first of all knwoing everything you need
           </h2>
       </section>
+      <main>
+        <section className="input-field">
+            <input type="text" name="input-for-details" placeholder="Start Typing" />
+            <button className="add-details-button">Add detail</button>
+        </section>
+      </main>
     </>
   )
 }
