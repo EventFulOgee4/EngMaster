9ca9ee06e7856b5497aa3c184e34308c19f0a030
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-01 12:03:58 +0400

    added some small new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import DetailsList from "./DetailsList"
+import ProjectSetup from "./ProjectSetup"
 
 export default function App(){
   const dots = React.useRef(null)
   const [details, setDetails] = React.useState([])
+  const [project, setProject] = React.useState("")
   const detailsList = details.map((detail) => {
     return <li key={detail}>{detail}</li>
   })
@@ -12,6 +14,8 @@ export default function App(){
     const dotLine = ".".repeat(200) + "\n"
     dots.current.textContent = dotLine.repeat(53)
   })
+
+
 
   function addDetail(formData){
     const newDetail = formData.get("details") 
@@ -35,7 +39,11 @@ export default function App(){
             <input type="text" name="details" placeholder="Start Typing" />
             <button className="add-details-button">Add detail</button>
         </form>
-        <DetailsList detailsList = {detailsList}/>
+        <DetailsList 
+        detailsList = {detailsList}
+        details = {details}
+        />
+        {project && <ProjectSetup project = {project} />}
       </main>
     </>
   )
