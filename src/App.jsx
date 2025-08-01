3495b654bf0dfb0f3a02e77cc4e204ef1ad7f7da
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-01 09:25:03 +0400

    added some new states + imported the new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import DetailsList from "./DetailsList"
 
 export default function App(){
   const dots = React.useRef(null)
+  const [details, setDetails] = React.useState([])
+  const detailsList = details.map((detail) => {
+    return <li key={detail}>{detail}</li>
+  })
 
   React.useEffect(() => {
-    const dotLine = ".".repeat(500) + "\n"
+    const dotLine = ".".repeat(200) + "\n"
     dots.current.textContent = dotLine.repeat(200)
   })
 
@@ -25,6 +30,7 @@ export default function App(){
             <input type="text" name="input-for-details" placeholder="Start Typing" />
             <button className="add-details-button">Add detail</button>
         </section>
+        <DetailsList detailsList = {detailsList}/>
       </main>
     </>
   )
