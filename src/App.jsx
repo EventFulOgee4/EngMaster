7195059e80df61a43d275706cda2036c75ab93cb
Javad Mammadaliyev <109952617+EventFulOgee4@users.noreply.github.com>
2025-08-01 13:46:54 +0400

    added the getProject prop to the file

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,7 @@ export default function App(){
         <DetailsList 
         detailsList = {detailsList}
         details = {details}
+        getProject = {getProject}
         />
         {project && <ProjectSetup project = {project} />}
       </main>
